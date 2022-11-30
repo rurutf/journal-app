98712cd219853244e7d41cf1367812d4668d989b
Inna Pavlyutenkova <i.pavlyutenkova@devspark>
2022-11-30 18:14:20 +0300

    install bootstrap, add button ui-components and add colors

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
+      <j-button :text="'Hello'"></j-button>
   </div>
   <router-view/>
 </template>
+
+<script>
+import JButton from "@/components/UI/J-button/J-button";
+export default {
+    components: {JButton}
+}
+</script>
 
 <style lang="scss">
 
---
+++ src/components/UI/J-button/J-button.vue
@@ -0,0 +1,39 @@
+<template>
+    <button
+        class="j-button"
+        :class="buttonClasses">
+        {{ text }}
+    </button>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    computed: {
+        buttonClasses() {
+            return {
+                'disabled': this.disabled,
+            }
+        }
+    },
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        text: {
+            type: String,
+            default: '',
+        },
+
+    }
+})
+export default class JButton extends Vue {}
+</script>
+
+<style lang="scss">
+.j-button {
+
+}
+</style>
